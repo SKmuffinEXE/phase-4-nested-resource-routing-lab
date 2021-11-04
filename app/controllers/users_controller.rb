0f265450821b49ca1aafee3af8e3d0b10ca7a0bf
David <DEvangelista54@gmail.com>
2021-11-04 10:52:20 -0400

    commit tree 807764a530bda1ca8fb95aa89f087ecbc53d47a1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
 
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  def index
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+  
+    end
+    render json: items, include: :user
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
   end
 
 end
